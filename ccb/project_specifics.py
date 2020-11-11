a0cdcb5cbf8a319c14ea75704d673529e56a0810
Quentin Chateau <quentin.chateau@gmail.com>
2020-11-11 13:25:57 +0100

    fix libunwind

--- ccb/project_specifics.py
+++ ccb/project_specifics.py
@@ -15,6 +15,7 @@ PROJECT_TAGS_BLACKLIST = {
     "pybind11": [re.compile(r"(.*)b[0-9]+$")],
     "lerc": [re.compile(r"^runtimecore_(.*)")],
     "xtensor": [re.compile(r"(.*)-binder[0-9]*$")],
+    "libunwind": [re.compile(r"^4\.0\.(6|7|9|10)$")],
 }
 
 # Project-based whitelist, a blacklist must not be defined
