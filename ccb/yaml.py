e401f6d6d7dd4a155f322eb7b8f57cfd228078f5
Quentin Chateau <quentin.chateau@gmail.com>
2020-11-18 11:36:17 +0100

    ccb: fix yaml format

--- ccb/yaml.py
+++ ccb/yaml.py
@@ -3,5 +3,6 @@ from ruamel.yaml.constructor import DoubleQuotedScalarString
 
 yaml = YAML()
 yaml.preserve_quotes = True
+yaml.indent(mapping=2, sequence=4, offset=2)
 
 DoubleQuotes = DoubleQuotedScalarString
