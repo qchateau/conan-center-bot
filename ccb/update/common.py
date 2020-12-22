263dc5a9d11c2a0972d780f2c9d566c80133f1a7
Quentin Chateau <quentin.chateau@gmail.com>
2020-12-22 13:11:09 +0100

    [ccb] reject version on warnings

--- ccb/update/common.py
+++ ccb/update/common.py
@@ -29,8 +29,10 @@ RE_TEST_ERRORS = [
     re.compile(r"^ERROR:.*(Invalid configuration.*)", re.M | re.S),
     re.compile(r"^ERROR:\s*(.*)", re.M | re.S),
 ]
+
 RE_ALREADY_PATCHED = re.compile(r"WARN:\s*(.*):\s*already patched", re.M)
-RE_CREATE_ERRORS = [RE_ALREADY_PATCHED]
+RE_WARNING = re.compile(r"WARN:\s*(.*)", re.M)
+RE_CREATE_ERRORS = [RE_WARNING]
 
 RE_CMAKELISTS_VERSION = re.compile(
     r"(cmake_minimum_required\s*\(\s*VERSION\s*)([0-9\.]+)(\s*\))", re.I
@@ -67,6 +69,11 @@ def get_test_details(output):
     if matches:
         patches = {m.group(1) for m in matches}
         return "Patch already applied:\n" + "\n".join(patches)
+
+    matches = list(RE_WARNING.finditer(output))
+    if matches:
+        warnings = [m.group(1) for m in matches]
+        return "Warnings during conan create:\n" + "\n".join(warnings)
 
     return "no details"
 
