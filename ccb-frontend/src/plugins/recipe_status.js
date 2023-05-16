c3218b19621ed3c8e7a737aa0d906ce49399c349
ericLemanissier <ericLemanissier@users.noreply.github.com>
2023-05-16 14:03:01 +0200

    make the frontend user data from fork

--- ccb-frontend/src/plugins/recipe_status.js
+++ ccb-frontend/src/plugins/recipe_status.js
@@ -1,6 +1,9 @@
 import axios from 'axios'
 
-const url = 'https://raw.githubusercontent.com/qchateau/conan-center-bot/status/prod/v1-update.json'
+const user = window.location.host.split('.')[0];
+const repo = window.location.pathname.split('/')[1];
+
+const url = `https://raw.githubusercontent.com/${user}/${repo}/status/prod/v1-update.json`
 
 export default {
   install (Vue) {
